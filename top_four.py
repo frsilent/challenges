681e519db5520fd78d04893e591fc3ac0369d0bd
Roland <rheintze@linux.com>
2013-02-12 16:50:26 +0300

    Adding any missed files

---
+++ top_four.py
@@ -0,0 +1,10 @@
+#Given an unordered list of N elements, write a function to find the top
+#four elements of the given list.
+#Make sure your function runs in linear time O(N).
+# @ Heintze, Darrell R.
+#   02/11/2013
+
+input_values = {}
+output_values = input_values[:3]
+
+for i in input_values:
