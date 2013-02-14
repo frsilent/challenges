8d8dbb5a0bdb582d266fec7f85b57ab8f0418ee7
Roland <rheintze@linux.com>
2013-02-14 14:46:43 -0500

    Added all test data to version control

--- circle_buffer.py
+++ circle_buffer.py
@@ -1,7 +1,8 @@
 #There are four types of commands.
 # 
 #"A"  n -  Append the following n lines to the buffer. If the buffer is full they replace the older entries.
-#"R"  n -  Remove first n elements of the buffer. These n elements are the ones that were added earliest among the current elements.
+#"R"  n -  Remove first n elements of the buffer. These n elements are the ones that were added earliest among the
+#          current elements.
 #"L"   - List the elements of buffer in order of their inserting time.
 #"Q"  - Quit.  
 # 
@@ -17,7 +18,8 @@
 # 
 #Output format :
 # 
-#Whenever  L command appears in the input, print the elements of buffer in order of their inserting time. Element that was added first should appear first. 
+# Whenever  L command appears in the input, print the elements of buffer in order of their inserting time. Element
+# that was added first should appear first.
 # 
 #Sample Input :
 # 
@@ -38,31 +40,33 @@
 #Fum
 
 #TODO: Write Test Buffer function in order to pass provided input to Buffer class
-def TestBuffer(self):
+def TestBuffer():
     """
     Function for the purpose of testing the Buffer class
     """
+    #input = std.in.readline()
+    #Buffer(input)
 
     testingBuffer = Buffer(500)
     testingBuffer.append(5)
     testingBuffer.remove(10)
 
-    newBuffer = testingBuffer.get()
 
 class Buffer:
     """Circular buffer class. Allows users to append, remove and list the contents."""
 
 
-    def __init__(self,size):
+    def __init__(self, size):
         """
         The new items are appended to the end and the order is retained i.e elements are placed in increasing order
-        of their insertion time. When the number of elements in the list elements exceeds the defined size, the older elements are overwritten.
+        of their insertion time. When the number of elements in the list elements exceeds the defined size,
+        the older elements are overwritten.
         """
-        self.size=max(min(size,10000))
-        self.data=[]
-        self.head=0
-        self.tail=0
-        full=False
+        self.size = max(min(size,10000))
+        self.data = []
+        self.head = 0
+        self.tail = 0
+        full = False
 
     def append(self,x):
         """
@@ -70,13 +74,14 @@ class Buffer:
         If the buffer is full, the oldest elements are overwritten (the head)
         """
         if self.data.len() > self.size:
-            self.data[self.head]=x
-            self.head+=1
+            self.full = True
+            self.data[self.head] = x
+            self.head += 1
         else:
             self.data.append(x)
-            self.tail+=1
+            self.tail += 1
 
-    def remove(self,removal_index):
+    def remove(self, removal_index):
         """
         Removes an element from the buffer
         """
