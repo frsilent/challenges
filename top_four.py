807cc0df675eb68c3de78cc433239fa26137884c
Roland Heintze <rheintze@linux.com>
2013-02-17 19:21:34 -0500

    This is done. Needs to be refactored for web submission but works on local input

--- top_four.py
+++ top_four.py
@@ -1,38 +1,8 @@
-# Given an unordered list of N elements, write a function to find the top four elements of the given list. Make sure your function runs in linear time O(N).
-# 
-#Input format :
-# 
-#First line of input contains N , number of elements in list.
-#Next N line , each contains a element.
-# 
-#Output format :
-# 
-#Print the top four elements of list.
-# 
-#Sample input :
-# 
-#8
-#6930886
-#-1692777
-#4636915
-#7747793
-#-4238335
-#9885386
-#9760492
-#6516649
-# 
-#Sample ouput :
-# 
-#9885386
-#9760492
-#7747793
-#6930886
-# 
-#Constraint :
-# 
-#N < 1000000.
-#all numbers will fit  in 32-bit integer.
-
-input_values = {}
-input_values.sort()
-output_values = input_values[:3]
+f = open('test_data/tf_input01.txt')
+count = f.readline()
+terms = []
+for i in range(int(count)):
+    terms.append(int(f.readline().strip()))
+terms.sort(reverse=True)
+for i in range(4):
+    print(terms[i])
