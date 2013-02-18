89d304359721ae2ceedfec7d9ffb97f197b2fbc5
Roland Heintze <rheintze@linux.com>
2013-02-17 19:11:37 -0500

    Added TODOs for last frustration

--- frequency_count.py
+++ frequency_count.py
@@ -1,42 +1,19 @@
-# Frequency Counting of Words / Top N words in a document.
-# 
 #Given N terms, your task is to find the k most frequent terms from given N terms.
-# 
-#Input format :
-# 
-#First line of input contains N, denoting the number of terms to add.
-#In each of the next N lines, each contains a term.
-#Next line contains k, most frequent terms.
-# 
 #Output format :
-# 
 #Print the k most frequent terms in descending order of their frequency. If two terms have same frequency print them in lexicographical order.
-# 
-#Sample input :
-# 
-#14
-#Fee
-#Fi
-#Fo
-#Fum
-#Fee
-#Fo
-#Fee 
-#Fee
-#Fo
-#Fi
-#Fi
-#Fo
-#Fum
-#Fee
-#3
-# 
-#Sample output :
-# 
-#Fee
-#Fo
-#Fi
-# 
-#Constraint :
-#0 < N < 300000 
-#0 < term length < 25.
+
+f = open('test_data/ft_input01.txt')
+terms = []
+for x in range(min(int(f.readline()),300000)):
+    terms.append(f.readline().strip())
+terms_dict = {}
+for item in terms:
+    terms_dict[item] = terms_dict.get(item, 0) + 1
+
+#TODO: Need to find a way to sort these so that it's sorted primarily by frequency & then alphabetical
+terms = sorted(terms_dict.items(), key=lambda item: item[1], reverse = True)
+
+terms.sort(key=lambda tup: tup[0])
+
+for term in range(int(f.readline())):
+    print terms[term][0]
