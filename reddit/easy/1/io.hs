b1ad26779ec68b1deaeeb6d5cf20b5a456050a5f
Roland <rheintze@linux.com>
2014-08-03 01:18:08 -0400

    Refactored hs solution & added second for py

--- reddit/easy/1/io.hs
+++ reddit/easy/1/io.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wall #-}
 {-|
 create a program that will ask the users name, age, and reddit username.
 have it tell them the information back, in the format:
@@ -6,19 +7,19 @@ for extra credit, have the program log this information in a file to be accessed
 -}
 
 
-greeting :: String -> String -> String -> String
---Your name is {}, you are {} years old, and your username is {}.
-greeting name age username = "Your name is " ++ name ++ ", you are "
+--Constructs a greeting based on passed arguments (format name, age, username)
+greeting :: (String,String,String) -> String
+greeting (name, age, username) = "Your name is " ++ name ++ ", you are "
     ++ age ++ " years old, and your username is " ++ username ++ "."
 
 
 main = do
-       putStrLn "What is your name? "
-       name <- getLine
-       putStrLn "What is your age? "
-       age <- getLine
-       putStrLn "What is your username? "
-       username <- getLine
+   putStrLn "What is your name? "
+   name <- getLine
+   putStrLn "What is your age? "
+   age <- getLine
+   putStrLn "What is your username? "
+   username <- getLine
 
-       let result = greeting name age username
-       putStrLn result
+   let result = greeting(name, age, username)
+   putStrLn result
